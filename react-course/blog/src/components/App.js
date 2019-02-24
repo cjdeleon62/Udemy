e17e70d98ed62f4d5c7bf068a722f1b34ff53186
Christopher De Leon <cjdeleon215@gmail.com>
2019-02-23 23:26:45 -0500

    finish redux thunk application

--- react-course/blog/src/components/App.js
+++ react-course/blog/src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PostList from './PostList';
 
 const App = () => (
-    <div className="ui-container">
+    <div className="ui container">
         <PostList />
     </div>
 );
